6358d4dc5f24dc3b2bb91d2737e84568d9f949bc
Kathleen_M <kathleen.malialin@gmail.com>
2020-02-05 11:29:21 -0300

    maj-welcome-blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        
         <style>
             html, body {
                 background-color: #fff;
