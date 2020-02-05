e8f3738cc5a3bbe5922de628d7432127b15145e1
Naiima973 <Naiima.C@hotmail.fr>
2020-02-05 15:43:21 -0300

    ivd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    El0_cake
+                    El0_cake00
                 </div>
 
                 <div class="links">
