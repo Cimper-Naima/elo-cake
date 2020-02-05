37b0ab8ed4e7fad80ed18997f6b7b3fd069a1623
Naiima973 <Naiima.C@hotmail.fr>
2020-02-05 11:06:57 -0300

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,10 +70,10 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Se connecter</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">S'inscrire</a>
                         @endif
                     @endauth
                 </div>
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    El0_cake
                 </div>
 
                 <div class="links">
