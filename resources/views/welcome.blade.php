3e4a2a39bc79ed685adeaa82e5ad832cfbe3cc20
Naiima973 <Naiima.C@hotmail.fr>
2020-02-11 10:02:54 -0300

    Modif-css-branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,11 @@
 @extends('layouts.layout')
 @section('content')
 
-    <!-- <div class="content">
-        <div class="title m-b-md">
-            El0_cake
-        </div>
-        <div class="links">
-            <a href="https://www.instagram.com/el0_cake/">Instagram</a>
-            <a href="https://laracasts.com">Facebook</a>
-            <a href="https://blog.laravel.com">Blog</a>
-        </div>
-    </div>
-</div> -->
+      <div class="col-sm-12" id="branding">
+          <h1 id=brand>
+              El0_Cake</h1>
+          <p id=slogan>
+              Happiness in the belly!</p>
+</div>
+		
 @endsection
