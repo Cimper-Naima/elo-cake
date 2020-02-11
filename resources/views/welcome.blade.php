6537514829c18a0da7cf7c58e7aea1b6139b3745
Kathleen_M <kathleen.malialin@gmail.com>
2020-02-11 10:19:16 -0300

    correct-push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.layout')
 @section('content')
 
-<<<<<<< HEAD
       <div class="col-sm-12" id="branding">
           <h1 id=brand>
               El0_Cake</h1>
@@ -9,18 +8,4 @@
               Happiness in the belly!</p>
 </div>
 		
-=======
-    <div class="content">
-        <div>
-            <h1 id="brand">El0_Cake</h1>
-         <p id="slogan">Happiness in the belly !</p>   
-</div>
-        <div id="social-icon" class="links">
-            <a href="https://www.instagram.com/el0_cake/">Instagram</a>
-            <!-- <a href="https://laracasts.com">Facebook</a>
-            <a href="https://blog.laravel.com">Blog</a> -->
-        </div>
-</div>
-
->>>>>>> 9958aac007df081520c37564f5fff3537ffe8c71
 @endsection
