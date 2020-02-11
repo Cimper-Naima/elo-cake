2787646bb696bb6c6f0dc5308a5c51221fde9d03
Naiima973 <Naiima.C@hotmail.fr>
2020-02-11 10:15:21 -0300

    add-css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts.layout')
 @section('content')
 
-<<<<<<< HEAD
-    <div class="content">
-        <div>
-            <h1 id="brand">El0_Cake</h1>
-         <p id="slogan">Happiness in the belly !</p>   
-</div>
-        <div id="social-icon" class="links">
-            <a href="https://www.instagram.com/el0_cake/">Instagram</a>
-            <!-- <a href="https://laracasts.com">Facebook</a>
-            <a href="https://blog.laravel.com">Blog</a> -->
-        </div>
-</div>
-
-=======
       <div class="col-sm-12" id="branding">
           <h1 id=brand>
               El0_Cake</h1>
@@ -22,5 +8,4 @@
               Happiness in the belly!</p>
 </div>
 		
->>>>>>> 3e4a2a39bc79ed685adeaa82e5ad832cfbe3cc20
 @endsection
