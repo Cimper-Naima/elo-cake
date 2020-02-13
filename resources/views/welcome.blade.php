06be34fd01166d0edd82492dd10ce3bbc5d72bf6
Kathleen_M <kathleen.malialin@gmail.com>
2020-02-13 09:07:00 -0300

    add-carrousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,34 +30,36 @@
 <section class="container">
 <div class="container-fluid">
 		
-<div id="myCarousel" class="carousel slide bg-inverse w-50 ml-auto mr-auto" data-ride="carousel">
+<div id="cakerousel" class="carousel slide bg-inverse w-50 ml-auto mr-auto" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#cakerousel" data-slide-to="0" class="active"></li>
+    <li data-target="#cakerousel" data-slide-to="1"></li>
+    <li data-target="#cakerousel" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner" role="listbox">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="{{asset('img/1.jpg')}}" alt="First slide">
+      <img class="d-block w-100" src="{{asset('img/cup.jpg')}}" alt="Cupcakes bleus">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="{{asset('img/2.jpg')}}" alt="Second slide">
+      <img class="d-block w-100" src="{{asset('img/cake.jpg')}}" alt="Cupcakes blancs">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="{{asset('img/3.jpg')}}" alt="Third slide">
+      <img class="d-block w-100" src="{{asset('img/event.jpg')}}" alt="Gâteau d'anniversaire">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#cakerousel" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+    <span class="sr-only">Précédent</span>
   </a>
-  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#cakerousel" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+    <span class="sr-only">Suivant</span>
   </a>
 </div>
-
 </div>
+</section>
+<!-- end - Slider Gourmandises -->
+
 <!-- jQuery library -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
@@ -79,6 +81,4 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 })
 </script>
-</section>
-<!-- end - Slider Gourmandises -->
 @endsection
